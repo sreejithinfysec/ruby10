e8e064ecfcb49293e483a35ccad6e91466bab3c4
eileencodes <eileencodes@gmail.com>
2016-03-05 10:16:41 -0500

    Add email to user, add styles to forms

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160215153647) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
