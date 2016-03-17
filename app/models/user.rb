cf3dd660334d7fcc69d8d3474de09368be50d81c
eileencodes <eileencodes@gmail.com>
2016-03-16 21:13:40 -0400

    Mitigate XSS in user website
    Validating user input before storing it in the database is another way
    to protect users from XSS attacks.
    
    This simple validator uses Ruby's built in URI parser to enure that the
    scheme provided is `http` or `https`, disallowing the `javascript`
    protocol.
    
    Always use a whitelist over a blacklist becasue it default deny protects
    the application from accidently allowing a dangerous protocol.
    
    Additionally, use a URI parser when possible over a regex. Regex is easy
    to get wrong and URI parsers are widely used and built specifically for
    validating URIs and it's harder to trick the URI parser into accepting a
    malformed URI than a regex.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  WHITELISTED_URI_SCHEMES = %w( http https )
+
   validates_presence_of :name
+  validate :check_uri_scheme
 
   def to_xml(options={})
     if options[:builder]
@@ -8,4 +11,16 @@ class User < ApplicationRecord
       "<name>#{name}</name>"
     end
   end
+
+  private
+    def check_uri_scheme
+      begin
+        uri = URI.parse(website)
+        unless uri.scheme && WHITELISTED_URI_SCHEMES.include?(uri.scheme.downcase)
+          errors.add :website, 'is not an allowed URI scheme'
+        end
+      rescue URI::InvalidURIError
+        errors .add :website, 'is not a valid URI'
+      end
+    end
 end
